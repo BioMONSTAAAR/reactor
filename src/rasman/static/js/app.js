0c5f80a791aeae63b14f544339cf3e30338ac5d7
Michael King <mikeking3@gmail.com>
2014-01-05 20:21:15 -0800

    made generic function for generating CSV strings

--- src/rasman/static/js/app.js
+++ src/rasman/static/js/app.js
@@ -1,4 +1,28 @@
-var History = {};
+var History = {
+    makeCSV: function(args){
+        //hardcoding the assumption that time is always the x axis
+        var that = this;
+        var headers = Array.prototype.slice.call(arguments);
+        headers.unshift('Timestamp');
+        var headersValid = headers.every(function(header){
+            return !!that[header];
+        });
+        if (!headersValid){
+            throw "Cannot generate CSV for nonexistent data stream.";
+        };
+
+        var lines = [headers.join(',') + '\n'];
+        for (var i = 0; i<this.Timestamp.length; i++){
+            var data = [];
+            for (var k = 0; k<headers.length; k++){
+                data.push(this[headers[k]][i]);
+            };
+            lines.push(data.join(','));
+        };
+        return lines.join('\n');
+    },
+};
+
 var getCSV = $.get('/api/history/', function processCSV(data){
     var lines = data.split(/\n+/).filter(function(x){
         return /^\w/.test(x);//avoid blank lines
